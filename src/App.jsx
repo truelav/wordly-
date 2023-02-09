9ea02f36dc0cff6d21e878fa0b6a820fe84695fd
unknown <madan.serghei@yahoo.com>
2023-02-09 10:02:38 +0200

    first commit of an empty React Vite app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>
+        Wordly
+      </h2>
+    </div>
+  )
+}
+
+export default App
